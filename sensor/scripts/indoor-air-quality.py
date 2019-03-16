885d9a2e2d719e33dab8b68f3dc7b3e5efdedb39
Chris Crocker-White <chriscw@balena.io>
2019-03-15 18:33:10 -0700

    Removed old readings.py and removed telegraf
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- sensor/scripts/indoor-air-quality.py
+++ sensor/scripts/indoor-air-quality.py
@@ -4,11 +4,15 @@
 # Adapted from: https://raw.githubusercontent.com/pimoroni/bme680-python/master/examples/indoor-air-quality.py
 import bme680
 import time
+from influxdb import InfluxDBClient
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 except IOError:
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+influx_client = InfluxDBClient('influxdb', 8086, database='balena-sense')
+influx_client.create_database('balena-sense')
 
 # These oversampling settings can be tweaked to
 # change the balance between accuracy and noise in
@@ -29,68 +33,75 @@ sensor.select_gas_heater_profile(0)
 
 start_time = time.time()
 curr_time = time.time()
-burn_in_time = 300
+burn_in_time = 60
 
 burn_in_data = []
 
-try:
-    # Collect gas resistance burn-in values, then use the average
-    # of the last 50 values to set the upper limit for calculating
-    # gas_baseline.
-    print('Collecting gas resistance burn-in data for 5 mins\n')
-    while curr_time - start_time < burn_in_time:
-        curr_time = time.time()
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            gas = sensor.data.gas_resistance
-            burn_in_data.append(gas)
-            print('Gas: {0} Ohms'.format(gas))
-            time.sleep(1)
 
-    gas_baseline = sum(burn_in_data[-50:]) / 50.0
+# Collect gas resistance burn-in values, then use the average
+# of the last 50 values to set the upper limit for calculating
+# gas_baseline.
+print('Collecting gas resistance burn-in data for 1 min\n')
+while curr_time - start_time < burn_in_time:
+    curr_time = time.time()
+    if sensor.get_sensor_data() and sensor.data.heat_stable:
+        gas = sensor.data.gas_resistance
+        burn_in_data.append(gas)
+        print('Gas: {0} Ohms'.format(gas))
+        time.sleep(1)
 
-    # Set the humidity baseline to 40%, an optimal indoor humidity.
-    hum_baseline = 40.0
+gas_baseline = sum(burn_in_data[-50:]) / 50.0
 
-    # This sets the balance between humidity and gas reading in the
-    # calculation of air_quality_score (25:75, humidity:gas)
-    hum_weighting = 0.25
+# Set the humidity baseline to 40%, an optimal indoor humidity.
+hum_baseline = 40.0
 
-    while True:
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            gas = sensor.data.gas_resistance
-            gas_offset = gas_baseline - gas
+# This sets the balance between humidity and gas reading in the
+# calculation of air_quality_score (25:75, humidity:gas)
+hum_weighting = 0.25
 
-            hum = sensor.data.humidity
-            hum_offset = hum - hum_baseline
+print('Starting loop...\n')
+while True:
+    if sensor.get_sensor_data() and sensor.data.heat_stable:
+        gas = sensor.data.gas_resistance
+        gas_offset = gas_baseline - gas
 
-            # Calculate hum_score as the distance from the hum_baseline.
-            if hum_offset > 0:
-                hum_score = (100 - hum_baseline - hum_offset)
-                hum_score /= (100 - hum_baseline)
-                hum_score *= (hum_weighting * 100)
+        hum = sensor.data.humidity
+        hum_offset = hum - hum_baseline
 
-            else:
-                hum_score = (hum_baseline + hum_offset)
-                hum_score /= hum_baseline
-                hum_score *= (hum_weighting * 100)
+        # Calculate hum_score as the distance from the hum_baseline.
+        if hum_offset > 0:
+            hum_score = (100 - hum_baseline - hum_offset)
+            hum_score /= (100 - hum_baseline)
+            hum_score *= (hum_weighting * 100)
 
-            # Calculate gas_score as the distance from the gas_baseline.
-            if gas_offset > 0:
-                gas_score = (gas / gas_baseline)
-                gas_score *= (100 - (hum_weighting * 100))
+        else:
+            hum_score = (hum_baseline + hum_offset)
+            hum_score /= hum_baseline
+            hum_score *= (hum_weighting * 100)
 
-            else:
-                gas_score = 100 - (hum_weighting * 100)
+        # Calculate gas_score as the distance from the gas_baseline.
+        if gas_offset > 0:
+            gas_score = (gas / gas_baseline)
+            gas_score *= (100 - (hum_weighting * 100))
 
-            # Calculate air_quality_score.
-            air_quality_score = hum_score + gas_score
+        else:
+            gas_score = 100 - (hum_weighting * 100)
 
-            print('Gas: {0:.2f} Ohms,humidity: {1:.2f} %RH,air quality: {2:.2f}'.format(
-                gas,
-                hum,
-                air_quality_score))
+        # Calculate air_quality_score.
+        air_quality_score = hum_score + gas_score
 
-            time.sleep(1)
+        measurements = [
+            {
+                'measurement': 'balena-sense',
+                'fields': {
+                    'temperature': float(sensor.data.temperature),
+                    'pressure': float(sensor.data.pressure),
+                    'humidity': float(sensor.data.humidity),
+                    'air_quality_score': float(air_quality_score)
+                }
+            }
+        ]
 
-except KeyboardInterrupt:
-    pass
+        influx_client.write_points(measurements)
+
+        time.sleep(10)

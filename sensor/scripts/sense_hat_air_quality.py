ba496f67ef687b10f8660397ee706a5030b80504
Chris Crocker-White <chriscw@balena.io>
2019-03-21 12:01:38 -0700

    Restructure to support both BME680 and Sense HAT
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

---
+++ sensor/scripts/sense_hat_air_quality.py
@@ -0,0 +1,45 @@
+def get_readings(sensor):
+    # The sense HAT does not include any way to obtain an air quality score via gas measurement,
+    # so we can create one using the temperature and humidity reading based on distance from ideal values
+    temperature_ideal = 20
+    temperature_worst_variance = 20
+
+    humidity_ideal = 40
+    humidity_worst_variance = 30
+    humidity_weighting = 0.25 # this means % of the AQ figure will be humidity, the rest will be temperature
+
+    # Get the current temperature and humidity readings
+    current_temperature = sensor.temperature
+    current_humidity = sensor.humidity
+
+    # Find out how far from the ideal the current values are
+    current_temperature_variance = abs(current_temperature - temperature_ideal)/temperature_worst_variance
+    # if the variance is greater than 1 (100%), set it to as a maximum
+    # note also that 100% here is the worst possible value
+    if current_temperature_variance > 1:
+        current_temperature_variance = 1
+
+    # Do the same thing for the humidity reading
+    current_humidity_variance = abs(current_humidity - humidity_ideal)/humidity_worst_variance
+    if current_humidity_variance > 1:
+        current_humidity_variance = 1
+
+    # Scale the current variance measurements in accordance with the weighting to calculate a percentage
+    # this gives us a score where 100 is the worst possible value
+    air_quality_score = (current_humidity_variance * humidity_weighting) + (current_temperature_variance * (1 - humidity_weighting))
+    air_quality_score = air_quality_score * 100 # convert to %
+
+    # Flip the score so that 100 is good and 0 is bad
+    air_quality_score = abs(air_quality_score - 100)    
+
+    return [
+        {
+            'measurement': 'balena-sense',
+            'fields': {
+                'temperature': float(current_temperature),
+                'pressure': float(sensor.pressure),
+                'humidity': float(current_humidity),
+                'air_quality_score': float(air_quality_score)
+            }
+        }
+    ]
---
+++ sensor/scripts/take_measurement.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python
+
+# This script detects for the presence of either a BME680 sensor on the I2C bus or a Sense HAT
+# The BME680 includes sensors for temperature, humidity, pressure and gas content
+# The Sense HAT does not have a gas sensor, and so air quality is approximated using temperature and humidity only.
+
+import sys
+import bme680
+import time
+from sense_hat import SenseHat
+from influxdb import InfluxDBClient
+
+readfrom = 'unset'
+
+# First, check to see if there is a BME680 on the I2C bus
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except IOError:
+    print 'BME680 not found on 0x76'
+else:
+    readfrom = 'bme680'
+    print 'Using BME680 for readings'
+    # Import the bme680 methods and nitialise the bme680 burnin
+    import bme680_air_quality
+    bme680_air_quality.start_bme680(sensor)
+    get_readings = bme680_air_quality.get_readings
+
+# If no BME680, is there a Sense HAT?
+if readfrom == 'unset':
+    try:
+        sensor = SenseHat()
+    except:
+        print 'Sense HAT not found'
+    else:
+        readfrom = 'sense-hat'
+        print 'Using Sense HAT for readings (no gas measurements)'
+        # Import the sense hat methods
+        import sense_hat_air_quality
+        get_readings = sense_hat_air_quality.get_readings
+
+# If this is still unset, no sensors were found; quit!
+if readfrom == 'unset':
+    sys.exit()
+
+# Create the database client, connected to the influxdb container, and select/create the database
+influx_client = InfluxDBClient('influxdb', 8086, database='balena-sense')
+influx_client.create_database('balena-sense')
+
+
+while True:
+    measurements = get_readings(sensor)
+    influx_client.write_points(measurements)
+    time.sleep(10)

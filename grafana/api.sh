f8a94ff392fb47029319cb8cc31107343991985d
Chris Crocker-White <chriscw@balena.io>
2019-06-07 17:12:41 -0700

    Change grafana endpoint to remove auth requirement
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- grafana/api.sh
+++ grafana/api.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
 GRAFANA_URL="127.0.0.1:$GF_SERVER_HTTP_PORT"
-GRAFANA_DEFAULT_USER="admin"
-GRAFANA_DEFAULT_PW="admin"
 
 count=0
 max_retries=10
@@ -13,7 +11,7 @@ while [[ "$count" < "$max_retries" ]]; do
         count=$(( "$count" + 1 ))
         sleep 10
     fi
-    if curl -sq -w "\n" -X PUT "http://$GRAFANA_DEFAULT_USER:$GRAFANA_DEFAULT_PW@$GRAFANA_URL/api/org/preferences" \
+    if curl -sq -w "\n" -X PUT "http://$GRAFANA_URL/api/user/preferences" \
         -H 'Accept-Encoding: gzip, deflate, br' -H 'X-Grafana-Org-Id: 1' -H \
             'Content-Type: application/json;charset=UTF-8' -H \ 'Accept: application/json' --data-binary \
                 '{"homeDashboardId":1,"theme":"","timezone":""}' --compressed; then

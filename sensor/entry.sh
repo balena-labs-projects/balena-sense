3dac8681b12e3c521106a5bb0fad6501aeee1e4f
Chris Crocker-White <chriscw@balena.io>
2019-06-19 13:55:46 +0100

    Introduced Telegraf to take readings and push to InfluxDB cloud
    Change-type: minor
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- sensor/entry.sh
+++ sensor/entry.sh
@@ -3,4 +3,4 @@ mkdir -p /data/sensor
 touch /data/sensor/bsec_iaq.state
 cp -n /usr/src/app/bsec_bme680_linux/bsec_iaq.config /data/sensor/bsec_iaq.config
 
-python /usr/src/app/scripts/take_measurement.py
+python /usr/src/app/scripts/sensor.py

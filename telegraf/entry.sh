36238f6fabad0b437923baa1f4ab5972ba87393b
Alan Boris <alanboris@gmail.com>
2019-11-27 01:30:51 -0500

    Adding MQTT functionality
    Change-type: minor
    Signed-off-by: Alan Boris <alan@balena.io>

--- telegraf/entry.sh
+++ telegraf/entry.sh
@@ -15,6 +15,18 @@ else
   sed -i '/ExternalInfluxDB/,/EndExternalInfluxDB/ { s/^##*//; s/^ ExternalInfluxDB$/# ExternalInfluxDB/; s/^ EndExternalInfluxDB/# EndExternalInfluxDB/ }' /etc/telegraf/telegraf.conf
 fi
 
+if [[ -z $TELEGRAF_MQTT_URL_PORT ]]; then
+  echo 'No Telegraf MQTT URL:Port specified - Telegraf MQTT output disabled'
+  sed -i '/MQTTOutput/,/EndMQTTOutput/ s/^#*/#/' /etc/telegraf/telegraf.conf
+elif [[ $TELEGRAF_MQTT_URL_PORT == "INTERNAL" ]]; then
+  TELEGRAF_MQTT_URL_PORT="mqtt:1883"
+  echo 'Internal MQTT server specified for Telegraf - enabling MQTT output to '$TELEGRAF_MQTT_URL_PORT
+  sed -i '/MQTTOutput/,/EndMQTTOutput/ { s/^##*//; s/^ MQTTOutput$/# MQTTOutput/; s/^ EndMQTTOutput/# EndMQTTOutput/ }' /etc/telegraf/telegraf.conf
+else
+  echo 'External MQTT server specified for Telegraf - enabling MQTT output to '$TELEGRAF_MQTT_URL_PORT
+  sed -i '/MQTTOutput/,/EndMQTTOutput/ { s/^##*//; s/^ MQTTOutput$/# MQTTOutput/; s/^ EndMQTTOutput/# EndMQTTOutput/ }' /etc/telegraf/telegraf.conf
+fi
+
 if [[ $DISABLE_INTERNAL_INFLUXDB == "TRUE" ]]; then
   echo 'Internal InfluxDB disabled'
   sed -i '/InternalInfluxDB/,/EndInternalInfluxDB/ s/^#*/#/' /etc/telegraf/telegraf.conf
